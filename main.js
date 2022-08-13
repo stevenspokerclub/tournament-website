503a06b4f95075bddb36dff0fadb96d7e066e925
David Krauthamer <david_krauthamer@protonmail.com>
2022-08-13 13:59:47 -0400

    Change the code formatting of most files

--- main.js
+++ main.js
@@ -13,41 +13,44 @@ var seconds;
 var startseconds;
 var timeElapsed = 0;
 var firstTime = true;
-var tableRow = 2;   
+var tableRow = 2;
 var oldbackground;
 
-startBtn.addEventListener('click',startClick,false);
-pauseBtn.addEventListener('click',pauseClick,false);
-inputseconds.addEventListener('keyup',function(e){
+startBtn.addEventListener("click", startClick, false);
+pauseBtn.addEventListener("click", pauseClick, false);
+inputseconds.addEventListener(
+  "keyup",
+  function (e) {
     e.preventDefault();
-    if (e.keyCode === 13){
-        startClick();
+    if (e.keyCode === 13) {
+      startClick();
     }
-},false);
-
+  },
+  false
+);
 
 //Initialize Timer at Largest Table Value
 setHTML(timerInnerTable[tableRow].childNodes[1].innerHTML * 60);
-tableRow+= 2;
+tableRow += 2;
 
-function startClick(){
-    //Insert value of seconds into variable for future math purposes and check that no other instance is running.
-    seconds = Number(inputminutes.value)*60 + Number(inputseconds.value);
+function startClick() {
+  //Insert value of seconds into variable for future math purposes and check that no other instance is running.
+  seconds = Number(inputminutes.value) * 60 + Number(inputseconds.value);
 
-    if (!running && seconds > 0){
-        running = true;
-        countDown(seconds);
-        startBtn.style.backgroundColor = "#C32026";
-        startBtn.style.border = "none";
-        startBtn.style.color = "#FFF";
-        pauseBtn.style.backgroundColor = "#FFF";
-        pauseBtn.style.border = "5px solid #C32026";
-        pauseBtn.style.color = "#000";
-         }
-    
+  if (!running && seconds > 0) {
+    running = true;
+    countDown(seconds);
+    startBtn.style.backgroundColor = "#C32026";
+    startBtn.style.border = "none";
+    startBtn.style.color = "#FFF";
+    pauseBtn.style.backgroundColor = "#FFF";
+    pauseBtn.style.border = "5px solid #C32026";
+    pauseBtn.style.color = "#000";
+  }
 }
-function pauseClick(){
-    if(running){
+
+function pauseClick() {
+  if (running) {
     clearInterval(timeElapsed);
     running = false;
     startBtn.style.backgroundColor = "#FFF";
@@ -56,75 +59,73 @@ function pauseClick(){
     pauseBtn.style.backgroundColor = "#C32026";
     pauseBtn.style.border = "none";
     pauseBtn.style.color = "#FFF";
+  }
+}
+
+function countDown(seconds) {
+  timeElapsed = setInterval(() => {
+    seconds--;
+    setHTML(seconds);
+
+    //Flicker for less than one minute
+    if (seconds < 60) {
+      if (seconds % 2 == 0) {
+        inputseconds.style.color = "white";
+      } else {
+        inputseconds.style.color = "black";
+      }
     }
-}
-function countDown(seconds){
 
-        timeElapsed = setInterval(() => {
-            seconds--;
-            setHTML(seconds);
+    //When finished reset and play sound
+    if (seconds == 0) {
+      endClick();
+      if (tableRow == 4) {
+        setHTML(timerInnerTable[tableRow].childNodes[1].innerHTML * 60);
+        timerInnerTable[tableRow - 2].style.backgroundColor = "rgba(0,0,0,.1)";
+        oldbackground = timerInnerTable[tableRow].style.backgroundColor;
+        timerInnerTable[tableRow].style.backgroundColor = "rgba(195,32,38,0.4)";
+      } else {
+        setHTML(timerInnerTable[tableRow].childNodes[1].innerHTML * 60);
+        timerInnerTable[tableRow - 2].style.backgroundColor = oldbackground;
+        oldbackground = timerInnerTable[tableRow].style.backgroundColor;
+        timerInnerTable[tableRow].style.backgroundColor = "rgba(195,32,38,0.4)";
+      }
 
-            //Flicker for less than one minute
-            if (seconds < 60){
-                if (seconds % 2 == 0){
-                    inputseconds.style.color = "white";
-                }
-                else {
-                    inputseconds.style.color = "black";
-                }
-            }
-
-            //When finished reset and play sound
-            if (seconds == 0){
-                endClick();
-                if (tableRow == 4){
-                    setHTML(timerInnerTable[tableRow].childNodes[1].innerHTML * 60);
-                    timerInnerTable[tableRow-2].style.backgroundColor = "rgba(0,0,0,.1)";
-                    oldbackground = timerInnerTable[tableRow].style.backgroundColor;
-                    timerInnerTable[tableRow].style.backgroundColor = "rgba(195,32,38,0.4)";
-                }
-                else {
-                    setHTML(timerInnerTable[tableRow].childNodes[1].innerHTML * 60);
-                    timerInnerTable[tableRow-2].style.backgroundColor = oldbackground;
-                    oldbackground = timerInnerTable[tableRow].style.backgroundColor;
-                    timerInnerTable[tableRow].style.backgroundColor = "rgba(195,32,38,0.4)";
-                }
-                
-                tableRow+= 2;
-                var a = new Audio('sound.mp3');
-                a.play();
-                setTimeout( function() {
-                }, 5000);
-                startClick();
-            }
-        }, 1000);
-}
-function endClick(){
-        seconds = 0;  
-        clearInterval(timeElapsed);
-        running = false;
-        firstTime = true;
-        inputminutes.value="";
-        inputseconds.value="";
-        minProgress.style.height = "0%";
-        secProgress.style.height = "0%";
-        startBtn.style.backgroundColor = "#FFF";
-        startBtn.style.border = "5px solid #C32026";
-        startBtn.style.color = "#000";
-        pauseBtn.style.backgroundColor = "#FFF";
-        pauseBtn.style.border = "5px solid #C32026";
-        pauseBtn.style.color = "#000";
-}
-function setHTML(seconds){
-        inputminutes.value = Math.floor(seconds/60);
-        inputseconds.value = seconds - Math.floor(seconds/60)*60;
-
-        if (firstTime){
-            startseconds = seconds;
-            firstTime = false;
-        }
-
-        minProgress.style.height = (seconds / startseconds * 100) + "%";
-        secProgress.style.height = (inputseconds.value / 60 * 100) + "%";
+      tableRow += 2;
+      var a = new Audio("sound.mp3");
+      a.play();
+      setTimeout(function () {}, 5000);
+      startClick();
+    }
+  }, 1000);
 }
 
+function endClick() {
+  seconds = 0;
+  clearInterval(timeElapsed);
+  running = false;
+  firstTime = true;
+  inputminutes.value = "";
+  inputseconds.value = "";
+  minProgress.style.height = "0%";
+  secProgress.style.height = "0%";
+  startBtn.style.backgroundColor = "#FFF";
+  startBtn.style.border = "5px solid #C32026";
+  startBtn.style.color = "#000";
+  pauseBtn.style.backgroundColor = "#FFF";
+  pauseBtn.style.border = "5px solid #C32026";
+  pauseBtn.style.color = "#000";
+}
+
+function setHTML(seconds) {
+  inputminutes.value = Math.floor(seconds / 60);
+  inputseconds.value = seconds - Math.floor(seconds / 60) * 60;
+
+  if (firstTime) {
+    startseconds = seconds;
+    firstTime = false;
+  }
+
+  minProgress.style.height = (seconds / startseconds) * 100 + "%";
+  secProgress.style.height = (inputseconds.value / 60) * 100 + "%";
+}
